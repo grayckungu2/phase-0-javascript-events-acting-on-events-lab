0c44e07a325d2119df264fed9b0434e9f6126e80
Grace kungu <grayckungu2@gmail.com>
2023-06-26 22:17:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+  
+  // Function to move the dodger to the right
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
